a3212074a0e401259c4b04b86a45f62d03ba18a1
Công Nam Trương <namtc07@MacBook-Pro-cua-Cong.local>
2024-07-24 22:03:15 +0700

    feat: config eslint

--- babel.config.js
+++ babel.config.js
@@ -4,6 +4,7 @@ module.exports = function (api) {
     presets: ['babel-preset-expo'],
     plugins: [
       'react-native-reanimated/plugin',
+      ['babel-plugin-react-docgen-typescript', { exclude: 'node_modules' }],
       [
         'module-resolver',
         {
